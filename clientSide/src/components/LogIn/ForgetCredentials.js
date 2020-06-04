ed11dd88c2bf927344cfec203807f096f3286d22
Skerd <skerd.xh@gmail.com>
2020-06-04 11:54:19 +0200

    Added mistakenly deleted files

---
+++ clientSide/src/components/LogIn/ForgetCredentials.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import Modal from "../Modals/Modal";
+
+class ForgetCredentials extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            showModal : false,
+            modalInfo : {
+                title: "",
+                body: ""
+            },  
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.editModal = this.editModal.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({showModal: !this.state.showModal});
+    }
+
+    editModal(e){
+        var whatModal = e.target.id;
+
+        if( whatModal === "resetPassword"){
+            this.setState({modalInfo:{ title: "Keni harruar fjalekalimin?", body: "Ju lutem kontaktoni sekretarine e institucionit ne te cilin punoni. Faleminderit."}})
+        }
+        else if( whatModal === "resetEmail"){
+            this.setState({modalInfo:{ title: "Keni harruar email-in?", body: "Ju lutem kontaktoni sekretarine e institucionit ne te cilin punoni. Faleminderit."}})
+        }
+        this.toggleModal();
+    }
+
+    componentDidMount(){
+        console.log("ForgetCredentials mounted");     
+    }
+
+    render(){
+        return (
+            <div>
+                <a href="#" className="form-text text-muted">
+                    <small id="resetPassword" onClick={this.editModal}>Keni harruar fjalëkalimin?</small>
+                </a>
+                <a  href="#" className="form-text text-muted">
+                    <small id="resetEmail" onClick={this.editModal}>Keni harruar email-in?</small>
+                </a>
+
+                <Modal show={this.state.showModal} handleClose={this.toggleModal} modalInfo={this.state.modalInfo} ></Modal>
+            </div>
+        )
+    }
+
+}
+
+export default ForgetCredentials;
